0fe0f4be5933f4abec36ccc5617bc5d4aa61a30a
Matias Miranda <matt.miranda89@gmail.com>
2025-01-20 03:39:14 -0300

    Commit #1: Expo basic config. We generate a random color when we touch the screen.

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState('#fff');
+
+  // Function that generates a random color based on hexadecimal numbers.
+  const generateBackgroundColor = () => {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    setBackgroundColor(color);
+  }
+
+  return (
+    <View style={{...styles.container, backgroundColor: backgroundColor}} onTouchStart={() => generateBackgroundColor()}>
+      <Text>Hello there</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
